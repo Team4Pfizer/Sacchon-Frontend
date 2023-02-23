1b54c1074dca4ee6d79c3fc1f5f7fceb84a1e4c9
zoe-netizen <ntaoutiz@csd.auth.gr>
2023-02-23 13:19:24 +0200

    bgl finished

--- src/app/components/bgl-store/bgl-store.component.ts
+++ src/app/components/bgl-store/bgl-store.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { MatFormFieldControl } from '@angular/material/form-field';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-bgl-store',
@@ -11,11 +12,13 @@ export class BglStoreComponent implements OnInit {
 
   createForm: FormGroup;
   response: any;
+  submitted = false;
+  router!: Router;
 
   constructor(private fb: FormBuilder) {
     this.createForm = this.fb.group({
-      name: new FormControl('', [Validators.required]),
-      job: ['', new FormControl('', [Validators.required, Validators.minLength(3)])]
+      datetime: new FormControl('', [Validators.required]),
+      bgl: new FormControl('', [Validators.required, Validators.min(0), Validators.max(1000)])
     });
   }
 
@@ -27,6 +30,7 @@ export class BglStoreComponent implements OnInit {
   }
 
   onSubmit() {
+    this.submitted = true;
     const data = {
       name: this.createForm.get('name')!.value,
       job: this.createForm.get('job')!.value

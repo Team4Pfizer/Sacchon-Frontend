09f63670fdb20918010dd4c17692966867ede832
zoe-netizen <ntaoutiz@csd.auth.gr>
2023-02-24 17:03:21 +0200

    Add profile data

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -1,3 +1,5 @@
+import { PatientData, BgMeasurement, DciMeasurement, Consultation } from './../../Interfaces';
+import { PatientDataHandlingService } from './../../services/patient-data-handling.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +9,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
-  constructor() { }
+  patientData!: PatientData;
+  bgMeasurements!: BgMeasurement[];
+  dciMeasurements!: DciMeasurement[];
+  consultations!: Consultation[];
+  displayedColumnsConsultations: string[] = ['id', 'dosage', 'medication', 'date'];
+  displayedColumnsBgMeasurements: string[] = ['id', 'data', 'date', 'time'];
+  displayedColumnsDciMeasurements: string[] = ['id', 'data', 'date'];
+
+  constructor(private patientService: PatientDataHandlingService) { }
 
   ngOnInit(): void {
+    this.patientService.getPatientData('michael.lawson@gmail.com').subscribe(data => {
+      this.patientData = data;
+      this.bgMeasurements = data.bgMeasurementList;
+      this.dciMeasurements = data.dciMeasurementList;
+      console.log(data);
+    });
+
+    this.patientService.getConsultations('michael.lawson@gmail.com').subscribe(data => {
+      this.consultations = data;
+      console.log(data);
+    });
   }
 
 }

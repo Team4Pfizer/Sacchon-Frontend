500a3f35606793aa542d8e8b5a9418a2e54bb472
zoe-netizen <ntaoutiz@csd.auth.gr>
2023-02-24 15:43:30 +0200

    Add services for MediDataVault

--- src/app/services/patient-data-handling.service.ts
+++ src/app/services/patient-data-handling.service.ts
@@ -1,5 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { PatientData } from './../Interfaces';
+import { Consultation } from './../Interfaces';
 
 @Injectable({
   providedIn: 'root'
@@ -8,13 +11,13 @@ export class PatientDataHandlingService {
 
   constructor(private http: HttpClient) { }
 
-  getPatientData(patientEmail: string) {
+  getPatientData(patientEmail: string): Observable<PatientData> {
     let url = 'http://localhost:8080/api/v1/medidatavault/myaccount/' + patientEmail;
-    return this.http.get(url);
+    return this.http.get<PatientData>(url);
   }
 
-  getConsultations(patientEmail: string) {
+  getConsultations(patientEmail: string): Observable<Consultation[]> {
     let url = 'http://localhost:8080/api/v1/medidatavault/consultations/' + patientEmail;
-    return this.http.get(url);
+    return this.http.get<Consultation[]>(url);
   }
 }

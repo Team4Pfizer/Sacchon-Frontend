1d833bc7fd6a41c88174a6122e55b63493f9fa0e
zoe-netizen <ntaoutiz@csd.auth.gr>
2023-02-26 19:24:52 +0200

    modify delete service

--- src/app/services/delete-data.service.ts
+++ src/app/services/delete-data.service.ts
@@ -1,3 +1,4 @@
+import { Observable } from 'rxjs';
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 
@@ -8,21 +9,19 @@ export class DeleteDataService {
 
   constructor(private http:HttpClient) { }
 
-  deleteBgMeasurement(measurementId: number, patientEmail: string) {
-    let url = 'http://localhost:4200/api/v1/medidatavault/bgmeasurement/' + patientEmail + '/' + measurementId;
-    const headers = new HttpHeaders({
-      'Content-Type': 'application/json',
-      'Access-Control-Allow-Origin': 'http://localhost:4200'
-    });
-    return this.http.delete(url, { headers });
+  deleteBgMeasurement(measurementId: number, patientEmail: string): Observable<any> {
+    const url = 'http://localhost:8080/api/v1/medidatavault/bgmeasurement/' + patientEmail + '/' + measurementId;
+    const headers = new HttpHeaders()
+      .set('Content-Type', 'application/json')
+      .set('crossDomain', 'true');
+    return this.http.delete(url, {headers: headers});
   }
 
-  deleteDciMeasurement(measurementId: number, patientEmail: string) {
-    let url = 'http://localhost:4200/api/v1/medidatavault/dcimeasurement/' + patientEmail + '/' + measurementId;
-    const headers = new HttpHeaders({
-      'Content-Type': 'application/json',
-      'Access-Control-Allow-Origin': 'http://localhost:4200'
-    });
-    return this.http.delete(url, { headers });
+  deleteDciMeasurement(measurementId: number, patientEmail: string): Observable<any> {
+    const url = 'http://localhost:8080/api/v1/medidatavault/dcimeasurement/' + patientEmail + '/' + measurementId;
+    const headers = new HttpHeaders()
+      .set('Content-Type', 'application/json')
+      .set('crossDomain', 'true');
+    return this.http.delete(url, {headers: headers});
   }
 }
